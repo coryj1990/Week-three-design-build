cfbaa3b7f4db09a4f9259049f8e542ef093359d9
Cory Johnson <cory.r.johnson@cox.net>
2016-07-05 09:50:39 -0500

    Got the 3 main blocks set up

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+<link type="text/css"; rel="stylesheet"; href="W3style.css"/>
+<title> My first website from OCS </title>
+</head>
+<body>
+
+<div class="bigcontain">
+	<div class="leftcontain"> Left </div>
+	<div class="midcontain"> Mid </div>
+	<div class="rightcontain"> Right </div>
+</div>
+
+
+</body>
+</html>
