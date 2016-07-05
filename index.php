cf10622cbf46fe769740cf56ccf09aec8ddb1987
Cory Johnson <cory.r.johnson@cox.net>
2016-07-05 12:06:42 -0500

    corrections to appearance and left side near complete

--- index.php
+++ index.php
@@ -7,9 +7,73 @@
 <body>
 
 <div class="bigcontain">
-	<div class="leftcontain"> Left </div>
-	<div class="midcontain"> Mid </div>
-	<div class="rightcontain"> Right </div>
+
+
+	<div class="leftcontain">
+		<div class="leftnavbar"> This is where search goes here are some more words</div>
+	<div class="leftopts">
+		<div class="opttitle">Main</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+
+		<div class="opttitle">Movie Circles</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+		<div class="leftbaropt">
+			<div class="leftbarsymb"></div>
+			<div class="leftbarchoice"></div>
+		</div>
+
+	</div>
+	</div>
+	<div class="midcontain">
+		<div class="topmidbar"> This is where top mid bar goes</div> 
+
+		<div class="midelements">
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+			<div class="theactualmidelements"></div>
+		</div>
+</div>
+
+	<div class="rightcontain">
+		<div class="righttopbar">forthetoprightcontents</div>
+
+		<div class="rightbotbar">forthdid it change text</div>
+
+	</div>
+
+
 </div>
 
 
